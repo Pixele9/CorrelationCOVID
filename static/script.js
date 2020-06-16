847329c5a978b3db4e76b10416451c3a822a8ef2
Luis Arturo García Gonzalez <lgarcia103@alumnos.uaq.mx>
2020-06-15 19:34:08 -0500

    *added* Persistant Dark theme

--- static/script.js
+++ static/script.js
@@ -1,11 +1,23 @@
 var checkbox = document.querySelector('input[name=theme]');
 
+theme = localStorage.getItem('theme');
+if (theme != null && theme != undefined){
+    document.documentElement.setAttribute('data-theme', theme);
+    if (theme == 'dark') {
+        checkbox.checked = true;
+    } else {
+        checkbox.checked = false;
+    }
+}
+
 checkbox.addEventListener('change', function() {
     if(this.checked) {
         trans()
+        localStorage.setItem('theme', "dark");
         document.documentElement.setAttribute('data-theme', 'dark')
     } else {
         trans()
+        localStorage.setItem('theme', "light");
         document.documentElement.setAttribute('data-theme', 'light')
     }
 })
@@ -17,11 +29,13 @@ let trans = () => {
     }, 1000)
 }
 
-
-let predictionButton = document.querySelector("#pedict-button")
+let resultDiv = document.querySelector("#result")
+let predictionButton = document.querySelector("#prediction-button")
 let valueToPredict = document.querySelector("#prediction-input")
 let illnessSelected = document.querySelector("#illness-select").value
 predictionButton.addEventListener("click", () => {
+    resultDiv.className = "animate__animated animate__fadeInRight";
+    resultDiv.setAttribute("style", "display: block");
     let options = {
         method: "POST",
         headers: {'Content-Type':'application/json'},
